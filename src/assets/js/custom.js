64059c56974a6b1dc192fcfb94dbcf237aecfbe0
Khoa Nguyen <khoa.nguyen@beesightsoft.com>
2020-02-05 16:04:36 +0700

    update

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,16 +1,5 @@
 
 $(document).ready(function(){
-    var owl = $('.intergrate-carousel');
-    owl.owlCarousel({
-        loop:true,
-        nav:true,
-        items: 6,
-        autoWidth:true,
-        margin:50,
-        dots:false,
-        navText:["<div class='nav-btn prev-slide'><span class='icon-angle-left'></span></div>","<div class='nav-btn next-slide'><span class='icon-angle-right'></span></div>"]
-    });
-
     var owls = $('.quote-carousel');
     owls.owlCarousel({
         loop:true,
@@ -18,23 +7,6 @@ $(document).ready(function(){
         nav:true,
         items: 1,
         dots:true,
-        navText:["<div class='nav-btn prev-slide'><span class='icon-angle-left'></span></div>","<div class='nav-btn next-slide'><span class='icon-angle-right'></span></div>"]
-    });
-
-    $(window).on("resize", function() {
-        var viewportWidth = $(window).width();
-        if (viewportWidth <= 575) {
-            $(".navbar-collapse").addClass("fixed-top sticky-top");
-        } else {
-            $(".navbar-collapse").removeClass("fixed-top sticky-top");
-        }
-    });
-    $(document).ready(function() {
-        var viewportWidth = $(window).width();
-        if (viewportWidth <= 575) {
-            $(".navbar-collapse").addClass("fixed-top sticky-top");
-        } else {
-            $(".navbar-collapse").removeClass("fixed-top sticky-top");
-        }
+        navText:["<div class='nav-btn prev-slide'><img src='../assets/images/prev_btn.png'></div>","<div class='nav-btn next-slide'><img src='../assets/images/next_btn.png'></div>"]
     });
 });
