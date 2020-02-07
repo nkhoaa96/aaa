8126150e1c6a188f17b9ccac99b4b688042e1365
Khoa Nguyen <khoa.nguyen@beesightsoft.com>
2020-02-07 20:06:28 +0700

    update

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,10 +1,40 @@
 
 $(document).ready(function(){
 
-    $("#reg-btn").click(function() {
-        $('html,body').animate({
-                scrollTop: $(".section-register").offset().top},
-            'slow');
+
+
+    // $("#reg-btn").click(function() {
+    //     $(".img-move").addClass('display_none');
+    //     $('html,body').animate({
+    //             scrollTop: $(".section-register").offset().top},
+    //         'slow');
+    // });
+
+    $("#btn-reg-form").click(function() {
+        var city = $('#city_input').val();
+        var name = $('#name_input').val();
+        var phone = $('#phone_input').val();
+        var email = $('#email_input').val();
+
+        $.ajax({
+            url: "https://docs.google.com/forms/d/e/1FAIpQLScnRbnsHKY3Umrj_pzEFHJMYNsoUBU2Z8jy7H5cmUcdkVF4gg/formResponse",
+            data: {
+                "entry.857326020": city,
+                "entry.1091257351": name,
+                "entry.866888737": phone,
+                "entry.1655299612": email,
+            },
+            type: "POST",
+            dataType: "xml",
+            statusCode: {
+                200: function () {
+                    $("#ex1").modal({
+                        fadeDuration: 100
+                    });
+                }
+            }
+        });
+
     });
 
     var owls = $('.quote-carousel');
